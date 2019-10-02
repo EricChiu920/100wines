9baea967dd5af3d1c5b2edd3b1dc7ad4960d22d9
Eric Chiu <EricChiu920@gmail.com>
2019-10-02 13:17:27 -0400

    add loaders for css/sass

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import App from './app';
+import '../styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  // ReactDOM.render(<App />, root);
+})
