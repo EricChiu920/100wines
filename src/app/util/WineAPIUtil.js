7b794ca23f0199db9290299b44c9b4ca0886c8c5
Eric Chiu <EricChiu920@gmail.com>
2019-10-03 14:40:56 -0400

    update api url

--- src/app/util/WineAPIUtil.js
+++ src/app/util/WineAPIUtil.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+const baseUrl = 'https://evening-basin-34525.herokuapp.com/api';
+
 export const fetchAllWines = () => {
-  // return axios.get('https://top-100-example.s3.amazonaws.com/t100_2018.json');
-  return axios.get('http://localhost:3001/api/');
+  return axios.get(`${baseUrl}`);
 };
 
 export const fetchWine = (id) => {
-  // return axios.get(`https://top-100-example.s3.amazonaws.com/${id}.json`);
-  return axios.get(`http://localhost:3001/api/${id}`);
+  return axios.get(`${baseUrl}/${id}`);
 };
